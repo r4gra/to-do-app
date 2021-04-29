24da67d6930ea97abf35eeb924055576e4cbae14
r4gra <real4graphic@gmail.com>
2021-04-30 01:13:29 +0200

    make it more understandable

--- jquery.js
+++ jquery.js
@@ -1,12 +1,28 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $(function(){
-    $("#add").on("click",function(){
+    $("button").hover(function(){
         //event handler
         var val =$("input").val();
-        if(val !==''){
-            var elem =$('<li></li>').text(val);
-            $(elem).append('<button class="rem">X</button>');
-            $("#mylist").append(elem);
-            $('input').val('');//clear the input
-        }
+  if(val !== '') {
+        var elem =$('<li></li>').text(val);
+        $(elem).append('<button class="rem">X</button>');
+        $("ol").append(elem);
+        $('input').val('');//clear the input
+      }
     });
+ 
 });
