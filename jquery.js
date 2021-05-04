8453a6ca065d6fe36d16347b8d74ead23b63bae5
r4gra <real4graphic@gmail.com>
2021-05-04 20:19:59 +0200

    task completion and refine code

--- jquery.js
+++ jquery.js
@@ -1,31 +1,47 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 $(function(){
-    $("button").hover(function(){
+    
+	  //add task
+    $('#add').hover(function(){
         //event handler
         var val =$("input").val();
   if(val !== '') {
         var elem =$('<li></li>').text(val);
-        $(elem).append('<button class="rem">X</button>');
+        $(elem).append('<p class="rem">X</p>');
         $("ol").append(elem);
         $('input').val('');//clear the input
+        
+
+          //task completion
+      $(elem).click(function(){
+       if($(this).css('text-decoration-line')=="none")
+           $(this).css('text-decoration-line', 'line-through');
+       else if($(this).css('text-decoration-line')=="line-through")
+
+        $(this).css('text-decoration-line','none');
+
+
+                      //$(this).toggleClass('complete');
+
+      }); //task complettion button
+
+
+     //remove task
         $('.rem').click(function(){
           $(this).parent().remove();
-        })
-      }
-    });
+    
+
+
+
+        }); //.rem click
+
+      
+
+
+
+ }//if
+    
+    });  //add task button 
+
+
  
-});
+}); //main
